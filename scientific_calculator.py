7dabd113357647d75cf16e5a3da1acceffc8a70a
Yati Vijay <yativijay.official@gmail.com>
2021-06-04 23:05:53 +0530

    Update scientific_calculator.py

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -1,23 +1,28 @@
+#import all libraries
+
 from tkinter import Tk
 from tkinter import StringVar,Entry,Button
 from math import pi,e,sin,cos,tan,log,log10,ceil,degrees,radians,exp,asin,acos,floor,sqrt
 
-
+#build a calculator
 class calculator:
     def __init__(self):
         window=Tk()
         window.title('Scientific Calculator - YATI')
-        window.configure(background="#000000")
+        window.configure(background="#000000")  #window background
         self.string=StringVar()
-        window.minsize(435,486)
+        
+        #window dimension
+        window.minsize(435,486) 
         window.maxsize(435,486)
         
+        #text frame
         entry=Entry(window,fg="#DCDCDC",textvariable=self.string,bd=9,width="66")
         entry.grid(row=0,column=0,columnspan=7,padx=2,pady=15,ipady=14)
         entry.configure(background="#696969")
         entry.focus()
         
-
+        #calculator functions list
         values = ["7","8","9","/","%","del","AC",
                   "4","5","6","*","=",
                   "1","2","3","-","(",")","sqrt",
@@ -25,7 +30,7 @@ class calculator:
                   "sin","cos","tan","abs","floor","ceil","pi",
                   "asin","acos","degree","radian","log10","log","e"
                   ]
-
+        #creating frame for functions button
         text=1
         i=0
         row=1
@@ -33,26 +38,32 @@ class calculator:
         for t in values:
             padx=10
             pady=10
+            
             if (i%7==0):
                 row=(i//7)+1
                 col=0
+      
             if t=="+/-":
                 btn=Button(window,height=2,width=4,bd=4,padx=padx,pady=pady, text=t ,command=lambda txt=t:self.interchange())
                 btn.grid(row=row,column=col,padx=2,pady=1)
                 btn.configure(background="#C0C0C0")
+                
             if t=="=":
                 btn=Button(window,height=2,width=4,bd=4,padx=70,pady=pady,text=t,command=lambda txt=t:self.equals())
                 btn.grid(row=row,column=col,columnspan=3,padx=2,pady=2)
                 btn.configure(background="#808080")
                 i=i+2
+                
             elif(t=='del'):
                 btn=Button(window,height=2,width=4,bd=4,padx=padx,pady=pady, text=t ,command=lambda txt=t:self.delete())
                 btn.grid(row=row,column=col,padx=1,pady=1)
                 btn.configure(background="#F4A460")
+                
             elif(t=='AC'):
                 btn=Button(window,height=2,width=4,bd=4,padx=padx,pady=pady,text=t,command=lambda txt=t:self.clearall())
                 btn.grid(row=row,column=col,padx=1,pady=1)
                 btn.configure(background="#D2691E")
+                
             else:
                 if t!="+/-":
                     btn=Button(window,height=2,width=4,bd=4,padx=padx,pady=pady,text=t ,command=lambda txt=t:self.addChar(txt))
@@ -64,7 +75,10 @@ class calculator:
 
             col=col+1
             i=i+1
+            
         window.mainloop()
+        
+    #modulus function
     def interchange(self):
         w=self.string.get()
         s=''
@@ -86,10 +100,11 @@ class calculator:
         else:
             self.string.set("INVALID INPUT")
         
-
+    #clear all function
     def clearall(self):
         self.string.set("")
-
+        
+    #get ans function
     def equals(self):
         result=""
         try:
@@ -99,12 +114,15 @@ class calculator:
             result="INVALID INPUT"
         self.string.set(result)
         
+    #append charater to create equation
     def addChar(self,char):
         self.string.set(self.string.get()+(str(char)))
         
+    #delete single charater
     def delete(self):
         self.string.set(self.string.get()[0:-1])
-            
+        
+#call function to get the GUI-calculator
 calculator()
 
 
